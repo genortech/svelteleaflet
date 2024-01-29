c3424cab0e3dd0c2eeac4c94563108cfc0c2538d
lumparis <genortech@gmail.com>
2024-01-29 19:28:59 +1100

    Added leaflet

---
+++ src/lib/leaflet/map.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+	import { browser } from '$app/environment';
+	import { onMount, onDestroy, setContext, createEventDispatcher, tick } from 'svelte';
+	import L from 'leaflet';
+	import 'leaflet/dist/leaflet.css';
+
+	export let bounds: L.LatLngBoundsExpression | undefined = undefined;
+	export let view: L.LatLngExpression | undefined = undefined;
+	export let zoom: number | undefined = undefined;
+
+	const dispatch = createEventDispatcher();
+	let map: L.Map | undefined;
+	let mapElement: HTMLElement;
+
+	onMount(() => {
+		if (!bounds && (!view || !zoom)) {
+			new Error('Must set bounds, or view and zoom.');
+		}
+		map = L.map(mapElement)
+			.on('zoom', (e) => dispatch('zoom', e))
+			.on('popupopen', async (e) => {
+				await tick();
+				e.popup.update();
+			});
+		L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+			attribution: `&copy;<a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>,&copy;<a href="https://carto.com/attributions" target="_blank">CARTO</a>`
+		}).addTo(map);
+	});
+
+	onDestroy(() => {
+		map?.remove();
+		map = undefined;
+	});
+
+	setContext('map', { getMap: () => map });
+
+	$: if (map) {
+		if (bounds) {
+			map.fitBounds(bounds);
+		} else if (view && zoom) {
+			map.setView(view, zoom);
+		}
+	}
+</script>
+
+<div class="h-full w-full" bind:this={map}>
+	{#if map}
+		<slot />
+	{/if}
+</div>
